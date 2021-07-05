292828edf7413dcf0d8097c5f06abd165d699f6a
Jelly Han <jellyhan@Jellys-MacBook-Pro.local>
2021-07-05 05:28:55 -0700

    first runnable app version

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import { Calculator } from 'component/calculator/Calculator';
 import './App.css';
 
 function App() {
-  const [result, setResult] = useState(0);
-  const updateResulsHandler = (val) => {
-    if (val === '') {
-      setResult('0');
-    } else setResult(val);
-  };
   return (
     <div>
       <Calculator />
